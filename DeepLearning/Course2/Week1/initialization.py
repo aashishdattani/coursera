c8f8654ea7a5733059bdc9962516f33dcdef3c36
Aashish Dattani <aashish.dattani@target.com>
2019-12-03 15:17:20 -0800

    Added Course2 Week1

---
+++ DeepLearning/Course2/Week1/initialization.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from DeepLearning.Course2.Week1.util.init_utils import compute_loss, forward_propagation, backward_propagation
+from DeepLearning.Course2.Week1.util.init_utils import update_parameters, predict, load_dataset
+
+
+def model(X, Y, learning_rate=0.01, num_iterations=15000, print_cost=True, initialization="he"):
+    grads = {}
+    costs = []  # to keep track of the loss
+    m = X.shape[1]  # number of examples
+    layers_dims = [X.shape[0], 10, 5, 1]
+
+    # Initialize parameters dictionary.
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        a3, cache = forward_propagation(X, parameters)
+
+        # Loss
+        cost = compute_loss(a3, Y)
+
+        # Backward propagation.
+        grads = backward_propagation(X, Y, cache)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Print the loss every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+
+    # plot the loss
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+def initialize_parameters_random(layers_dims):
+    np.random.seed(3)  # This seed makes sure your "random" numbers will be the as ours
+    parameters = {}
+    L = len(layers_dims)  # integer representing the number of layers
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1])  * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1  # integer representing the number of layers
+
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * pow(2 / layers_dims[l - 1], 0.5)
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+    return parameters
+
+
+if __name__ == "__main__":
+    train_X, train_Y, test_X, test_Y = load_dataset()
+    parameters = model(train_X, train_Y, initialization="he")
+    print("On the train set:")
+    predictions_train = predict(train_X, train_Y, parameters)
+    print("On the test set:")
+    predictions_test = predict(test_X, test_Y, parameters)
